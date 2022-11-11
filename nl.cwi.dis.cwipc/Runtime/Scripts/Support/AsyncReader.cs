bce2e4f7ea31b07847f9374dba059b4bcd7a4a6f
Jack Jansen <Jack.Jansen@cwi.nl>
2022-11-11 13:12:11 +0100

    Moved workers here from VRTCore

---
+++ nl.cwi.dis.cwipc/Runtime/Scripts/Support/AsyncReader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cwipc
+{
+    public abstract class AsyncReader : AsyncWorker
+    {
+        public virtual void SetSyncInfo(SyncConfig.ClockCorrespondence _clockCorrespondence)
+        {
+            if (_clockCorrespondence.streamClockTime != _clockCorrespondence.wallClockTime)
+            {
+                Debug.LogWarning($"{Name()}: SetSyncInfo({_clockCorrespondence.wallClockTime}={_clockCorrespondence.streamClockTime}) called but not implemented in this reader");
+            }
+        }
+    }
+}
---
+++ nl.cwi.dis.cwipc/Runtime/Scripts/Support/AsyncWriter.cs
@@ -0,0 +1,13 @@
+namespace Cwipc
+{
+    public abstract class AsyncWriter : AsyncWorker
+    {
+        public AsyncWriter() : base()
+        {
+        }
+        public virtual SyncConfig.ClockCorrespondence GetSyncInfo()
+        {
+            return new SyncConfig.ClockCorrespondence();
+        }
+    }
+}
